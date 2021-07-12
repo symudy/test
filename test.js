858070d873fc0f0251558eedb91a02e92bc42c92
symudy <87005073+symudy@users.noreply.github.com>
2021-07-12 10:08:27 +0200

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-github_token: "ghp_mQKNJGOlqeZnUk3555Zaqitjoeoeoeoeoeo"
-slack_webhook: "https://hooks.slack.com/services/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"
-
-https://gtvault.webhook.office.com/webhookb2/90jvoif-8856-6532-5463-asdfth644t4efq@4gebrtheb5-5y6g-88gg-t5t5g-4r4fe34f3f337/IncomingWebhook/39j39jd93j3p9djqp9dkqp0/cdfbnflkwendlkdlkwdmqwed
-
-github_token: "ghp_oiuytGOlqeZnUk3cIFZaHGFDS8CQ1P4Vi4k8"
-
-github_2token: 'ghp_taw7LbFCF2bppbqdsETW20ryBiZEFGT254LK'
